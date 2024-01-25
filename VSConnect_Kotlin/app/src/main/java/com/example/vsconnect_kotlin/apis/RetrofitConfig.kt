b8b1a7f996a1b8382de5379a39538bd9df45d843
alcides Paz <alcidesnp@hotmail.com>
2024-01-24 21:50:10 -0300

    Adicionado API e alteração de projeto

--- VSConnect_Kotlin/app/src/main/java/com/example/vsconnect_kotlin/apis/RetrofitConfig.kt
+++ VSConnect_Kotlin/app/src/main/java/com/example/vsconnect_kotlin/apis/RetrofitConfig.kt
@@ -2,7 +2,7 @@ package com.example.vsconnect_kotlin.apis
 
 class RetrofitConfig {
     companion object{
-        fun obterInstanciaRetrofit(url: String = "") : Retrofit{
+        fun obterInstanciaRetrofit(url: String = "http://172.16.52.84:8099/") : Retrofit{
             return Retrofit.Builder()
                 .baseUrl(url)
                 .addConverterFactory(GsonConverterFactory.create())
